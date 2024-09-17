bd8416018a46956d19db39517b04462934cb6e25
apola <yan_1whun1@hotmail.com>
2024-09-17 20:25:10 +0400

    demo for scanning lidar

--- download_files.bash
+++ download_files.bash
@@ -14,7 +14,7 @@ if [ "$d_bag" != "${d_bag#[Yy]}" ] ;then
   unzip multi_depth_camera_images.zip
 fi
 
-echo -n "Do you want to download multilayer lidar bag files (120MB)? (Y/N):"
+echo -n "Do you want to download multilayer lidar (leishen c16) bag files (120MB)? (Y/N):"
 read m_bag
 if [ "$m_bag" != "${m_bag#[Yy]}" ] ;then 
   echo "Download bag"
@@ -24,4 +24,16 @@ if [ "$m_bag" != "${m_bag#[Yy]}" ] ;then
       'https://drive.usercontent.google.com/download?id='1MnvlZkYrqKZwCJ74LMuCD1zh4owUq6I-'&confirm='$(<confirm.txt)
   rm -f confirm.txt cookies.txt
   unzip multilayer_spinning_lidar_pointcloud.zip
+fi
+
+echo -n "Do you want to download multilayer lidar (unitree g4) bag files (25MB)? (Y/N):"
+read u_bag
+if [ "$u_bag" != "${u_bag#[Yy]}" ] ;then 
+  echo "Download bag"
+  cd ~/dddmr_bags && curl -L -c cookies.txt 'https://drive.usercontent.google.com/uc?export=download&id='1QVMqS-rCi5Q96LgwVDxc-jD5VhVjwohn- \
+      | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
+  curl -L -b cookies.txt -o unitree_lidar_point_cloud.zip \
+      'https://drive.usercontent.google.com/download?id='1QVMqS-rCi5Q96LgwVDxc-jD5VhVjwohn-'&confirm='$(<confirm.txt)
+  rm -f confirm.txt cookies.txt
+  unzip unitree_lidar_point_cloud.zip
 fi
