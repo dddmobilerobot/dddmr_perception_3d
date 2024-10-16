0dddb5acb9cc852bdeee8e2a02208de429532f56
apola <yan_1whun1@hotmail.com>
2024-10-16 10:34:22 +0400

    nothing layer for easy bringup

---
+++ plugins/nothing_layer.cpp
@@ -0,0 +1,71 @@
+/*
+* BSD 3-Clause License
+
+* Copyright (c) 2024, DDDMobileRobot
+
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+
+* 1. Redistributions of source code must retain the above copyright notice, this
+*    list of conditions and the following disclaimer.
+
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+
+* 3. Neither the name of the copyright holder nor the names of its
+*    contributors may be used to endorse or promote products derived from
+*    this software without specific prior written permission.
+
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include <perception_3d/nothing_layer.h>
+
+PLUGINLIB_EXPORT_CLASS(perception_3d::NothingLayer, perception_3d::Sensor)
+
+namespace perception_3d
+{
+
+NothingLayer::NothingLayer(){
+}
+
+NothingLayer::~NothingLayer(){
+}
+
+void NothingLayer::onInitialize()
+{ 
+  sensor_current_observation_.reset(new pcl::PointCloud<pcl::PointXYZI>);
+}
+
+void NothingLayer::selfMark(){
+}
+
+void NothingLayer::selfClear(){
+}
+
+void NothingLayer::resetdGraph(){
+}
+
+double NothingLayer::get_dGraphValue(const unsigned int index){
+  return 1000000.0;
+}
+
+bool NothingLayer::isCurrent(){
+  current_ = true;
+  return current_;
+}
+
+pcl::PointCloud<pcl::PointXYZI>::Ptr NothingLayer::getObservation(){
+  return sensor_current_observation_;
+}
+
+}//end of name space
